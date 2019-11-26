21bc838dcac1ff768c453661bc211ca4ed0ce2e0
viet20062000 <57929687+viet20062000@users.noreply.github.com>
2019-11-26 11:26:59 +0700

    Create showinfor.php

---
+++ showinfor.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $db = parse_url(getenv("DATABASE_URL"));
+    $pdo = new PDO("pgsql:" . sprintf(
+        "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+        $db["host"],
+        $db["port"],
+        $db["user"],
+        $db["pass"],
+        ltrim($db["path"], "/")
+    ));
+    $sql ="Select * from product";
+    //compile
+    $stmt = $pdo->prepare($sql);
+    //execute
+    $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    $stmt->execute();
+    $resultSet=$stmt->fetchAll();
+    ?>
+    <ul>
+    <?php 
+    foreach($resultSet as $row)
+    {
+        echo "<li>" . $row[$name].'--'.$row["price"]."</li>";
+    }
+    ?>
+    <li></li>
+    </ul>
+</body>
+</html>
