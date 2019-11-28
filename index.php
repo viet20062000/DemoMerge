10d898142c181d96a4c347bc0ea494076d4ae821
viet20062000 <57929687+viet20062000@users.noreply.github.com>
2019-11-28 09:20:33 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,9 @@
-<!DOCTYPE html>
-<html>
 <body>
-<h1> Create Product </h1>
-<form action="insertProduct.php" method="post">
-Name <input type="text" name="name" >
-<br>
-Price <input type="text" name="name" >
-<br>
-<input type="submit" value="Insert">
-</form>
-<a href="showinfor.php">Click here to view product infor!</a>
-
-</body>
-</html>
+	<h1>Create Product </h1>
+	<form action="insertProduct.php" method="post">
+		Name <input type="text" name="name" >
+		<br>
+		Price <input type="text" name="price">
+		<br>
+		<input type="submit" value="Insert">
+	</form>
