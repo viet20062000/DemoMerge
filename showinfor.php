1609247ff3a210f23d0affd774867d954ea1b111
viet20062000 <57929687+viet20062000@users.noreply.github.com>
2019-11-28 11:17:30 +0700

    Update showinfor.php

--- showinfor.php
+++ showinfor.php
@@ -36,9 +36,9 @@
     {
         echo "<tr>" ."<td>". $row["productid"]."</td>".
         "<td>".$row["name"]."</td>".
-        "<td>".$row["price"]."</td>"."</tr>".
-        "<td> <button onclick="location.href='deleteProduct.php?id=.'$row["productid"]''" type="button">".
-        Delete."</button>";
+        "<td>".$row["price"]."</td>".
+        "<td> <button onclick='location.href='deleteProduct.php?id='.$row["productid"].'' type='button'>".
+        Delete."</button></tr>";
     }
     ?>
     </table>
