8058a7f0b50f2018ffe6c4864fa5113d2587c8e5
viet20062000 <57929687+viet20062000@users.noreply.github.com>
2019-11-28 09:16:57 +0700

    Update insertProduct.php

--- insertProduct.php
+++ insertProduct.php
@@ -1,23 +1,22 @@
 <?php
-$db = parse_url(getenv("DATABASE_URL"));
-$pdo = new PDO("pgsql:" . sprintf(
-    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
-    $db["host"],
-    $db["port"],
-    $db["user"],
-    $db["pass"],
-    ltrim($db["path"], "/")
-));
-$name =$_POST['name'];
-$price =$_POST['price'];
-$data = [
-    'name'=>$name,
-    'price'=>$price,
-];
-$sql ="insert into product (name, price) values (:name,:price)");
-//compile
-$stmt = $pdo->prepare($sql);
-//execute
-$stmt->execute($data);
-echo("insert ok!");
+    $db = parse_url(getenv("DATABASE_URL"));
+    $pdo = new PDO("pgsql:" . sprintf(
+        "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+        $db["host"],
+        $db["port"],
+        $db["user"],
+        $db["pass"],
+        ltrim($db["path"], "/")
+    ));    
+    $name = $_POST["name"];
+    $price = $_POST["price"];
+    $data = [
+        'name' => $name,
+        'price' => $price
+    ];
+    $stmt =  
+        $pdo->prepare("INSERT INTO product(name, price) VALUES (:name,:price)");	
+    $stmt->execute($data);
+    echo("insert ok!");
+ 
 ?>
